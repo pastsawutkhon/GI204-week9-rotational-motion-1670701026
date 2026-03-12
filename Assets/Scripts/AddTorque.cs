f36f49c0e5965e88004e451778c302b12245a422
pastsawutkhon <pastsawut.khon@bumail.net>
2026-03-12 10:17:58 +0700

    Add code to both cube

--- Assets/Scripts/AddTorque.cs
+++ Assets/Scripts/AddTorque.cs
@@ -1,19 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AddTorque : MonoBehaviour
 {
     public float torquePower;
+    Rigidbody rb;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque(Vector3.right * torquePower * Time.deltaTime);
+        }
     }
 }
